765f64f5990f20298ff5ccb57750e455e5e736f8
emddeveloper <emd.developer@gmail.com>
2020-09-21 16:27:05 +0530

    Working facebook login with react.env file exclude

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react"
+import "./App.css"
+import Axios from "axios"
+import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom"
+import FacebookLogin from "react-facebook-login"
+import GoogleLogin from "react-google-login"
 function App() {
+  //const history = useHistory()
+  async function getFacebookurl(fbuserdata) {
+    const params = {
+      ...fbuserdata,
+      type: "SOCIAL"
+    }
+    try {
+      const response = await Axios.post("/signup", params)
+      console.log(response.data)
+    } catch (e) {
+      console.log("Something went wrong" + e)
+    }
+  }
+  const [fbdata, setFbdata] = useState({})
+  const responseFacebook = response => {
+    setFbdata(response)
+    getFacebookurl(response)
+  }
+  const responseGoogle = response => {
+    console.log(JSON.stringify(response))
+  }
+  if (!process.env.REACT_APP_FB_API_ID || !process.env.REACT_APP_GOOGLE_CLIENT_ID) {
+    alert("Please include .env file for your API keys")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Switch>
+            <Route to="/" exact>
+              <>
+                <FacebookLogin appId={process.env.REACT_APP_FB_API_ID} autoLoad={false} fields="name,email,picture" callback={responseFacebook} />
+                <div>Welcome</div>
+                <GoogleLogin clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle} cookiePolicy={"single_host_origin"} />
+              </>
+            </Route>
+            <Route to="/failed">
+              <div>"Sorry"</div>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
